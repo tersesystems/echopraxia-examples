2cb5e68f20b69ed9a6928e435e369414972f9ca2
Will Sargent <will.sargent@gmail.com>
2022-03-07 11:35:30 -0800

    Use version

--- custom-field-builder/src/main/java/com/example/Main.java
+++ custom-field-builder/src/main/java/com/example/Main.java
@@ -1,5 +1,14 @@
 package com.example;
 
+import static com.jayway.jsonpath.Criteria.where;
+import static com.jayway.jsonpath.Filter.filter;
+
+import com.jayway.jsonpath.Filter;
+import com.tersesystems.echopraxia.Condition;
+
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
   // Use a logger with a custom field builder that can log a Person object.
@@ -10,6 +19,12 @@ public class Main {
     abe.setFather(new Person("Bert", 35, "keyboards"));
     abe.setMother(new Person("Candace", 30, "iceskating"));
 
-    logger.info("Hi there {}", fb -> fb.only(fb.person("person", abe)));
+    Condition abeCondition =
+        (level, context) -> {
+          // Language Injections in IntelliJ IDEA and add LoggingContext
+          Number age = context.find(Number.class, "$.person.mother.age");
+          return age.equals(30);
+        };
+    logger.info(abeCondition, "Hi there {}", fb -> fb.only(fb.person("person", abe)));
   }
 }
