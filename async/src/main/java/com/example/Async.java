4424c6b66466f2e0866264dfe09a07e755352252
Will Sargent <will.sargent@gmail.com>
2022-04-29 20:56:43 -0700

    update

--- async/src/main/java/com/example/Async.java
+++ async/src/main/java/com/example/Async.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.tersesystems.echopraxia.api.Condition;
+import com.tersesystems.echopraxia.api.Value;
 import com.tersesystems.echopraxia.async.AsyncLogger;
 import com.tersesystems.echopraxia.async.AsyncLoggerFactory;
 import java.util.concurrent.ExecutorService;
@@ -38,12 +39,21 @@ public class Async {
 
   public static void main(String[] args) throws InterruptedException {
     System.out.println("BEFORE logging block");
+
     for (int i = 0; i < 10; i++) {
+      // You can put MDC in
+      org.slf4j.MDC.put("contextKey", "" + i);
+
+      Condition c = (l, ctx) -> ctx.findString("$.contextKey").filter(key -> key.equals("5")).isPresent();
+      // and have it available as fields when you use `withThreadContext()`
+      logger.withThreadContext().info(c, "Message prints out on contextKey=5");
+
       // This should take no time on the rendering thread :-)
-      logger.info("Prints out after expensive condition");
+      int finalI = i;
+      logger.info("Prints out after expensive condition {}", fb -> fb.number("i", finalI));
     }
     System.out.println("AFTER logging block");
     System.out.println("Sleeping so that the JVM stays up");
-    Thread.sleep(1001L * 10L);
+    Thread.sleep(1001L * 20L);
   }
 }
